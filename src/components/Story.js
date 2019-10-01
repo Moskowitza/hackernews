3330232457a8412f8730a698a5be2d98b66e98a1
moskowitza <moskowitzaaron@mac.com>
2019-10-01 14:59:26 -0400

    styles

---
+++ src/components/Story.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React, { useState, useEffect, memo } from "react";
+import { getStory } from "../services/hnApi";
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement
+} from "../styles/StoryStyles";
+// import { mapTime } from "../mappers/mapTime";
+
+export const Story = memo(function Story({ storyId }) {
+  const [story, setStory] = useState({});
+
+  useEffect(() => {
+    getStory(storyId).then(data => data && data.url && setStory(data));
+  }, []);
+  const { url, title, by, time } = story;
+  return story && story.url ? (
+    <StoryWrapper data-testid="story">
+      <StoryTitle>
+        <a href={url}>{title}</a>
+      </StoryTitle>
+      <StoryMeta>
+        <span data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {by}
+        </span>
+        <span data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+          {time}
+          {/* {mapTime(time)} */}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
+  ) : null;
+});
+
+export default Story;
