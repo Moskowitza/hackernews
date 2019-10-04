890d99edcfe2ef2ea1a5ebfaf410b8ec92f32a95
moskowitza <moskowitzaaron@mac.com>
2019-10-04 07:14:59 -0400

    Updates

---
+++ precache-manifest.c1e87e35f6d68857efe8ed1e6a8d3058.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fbe6bec666d74a1f0b0af30957604e",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "2774a5971f8f92ca9c52",
+    "url": "/hackernews/static/js/2.70265f37.chunk.js"
+  },
+  {
+    "revision": "2358b3e7c9992777e035",
+    "url": "/hackernews/static/js/main.2be03710.chunk.js"
+  },
+  {
+    "revision": "1a479bce2e5e3ae006d8",
+    "url": "/hackernews/static/js/runtime-main.e191b2fc.js"
+  }
+]);
